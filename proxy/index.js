a30857f1627a34b734d3d96c6c897b1ac039ef41
ardydj <ardydj07@gmail.com>
2020-10-07 12:04:59 -0700

    Implement HTML File

--- proxy/index.js
+++ proxy/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const app = express()
-const port = 3001
-const path
+const app = express();
+const port = 3001;
+const path = require('path');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
