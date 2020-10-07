0821dc1540b4abbb1bb790323f021ee86ef82375
ardydj <ardydj07@gmail.com>
2020-10-07 11:53:58 -0700

    Implement proxy server file

---
+++ proxy/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3001
+const path
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.static('proxy'))
+app.use('/listing/:itemId', express.static(path.join(__dirname, 'index.html')))
+
+app.get('/', (req, res) => {
+  res.redirect('/listing/1');
+})
+
+app.listen(port, () => {
+  console.log(`Proxy listening at http://localhost:${port}`);
+})
