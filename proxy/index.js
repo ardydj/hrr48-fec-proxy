9273b538942e409adaf51891a39eb211257966c1
ardydj <ardydj07@gmail.com>
2020-10-07 15:25:25 -0700

    Render Carousel Module On Proxy

--- proxy/index.js
+++ proxy/index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express();
 const port = 3001;
 const path = require('path');
+const cors = require('cors');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use(express.static('proxy'))
 app.use('/listing/:itemId', express.static(path.join(__dirname, 'index.html')))
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.redirect('/listing/1');
